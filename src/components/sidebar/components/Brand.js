30dbf972d36ce5932c6568f3a3a6780783eeb854
gorfadumesh <gorfadumesh1213@gmail.com>
2024-06-26 20:58:42 +0530

    made some changes

--- src/components/sidebar/components/Brand.js
+++ src/components/sidebar/components/Brand.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // Chakra imports
-import { Flex, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Image, useColorModeValue } from "@chakra-ui/react";
 
 // Custom components
 import { HorizonLogo } from "components/icons/Icons";
@@ -13,7 +13,8 @@ export function SidebarBrand() {
 
   return (
     <Flex align='center' direction='column'>
-      <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
+      {/* <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} /> */}
+      <Image height={"70%"} p={"10px"} src={"https://www.unlimitedhimalaya.com/back_end/images/logo.png"} alt="" />
       <HSeparator mb='20px' />
     </Flex>
   );
